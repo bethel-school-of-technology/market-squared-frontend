069c1c7c4cb65203cae8c122b66bfae4d32ae3f4
Talin Crow <crowtech7@gmail.com>
2020-08-15 20:29:22 -0500

    Fixed connection issue from previous merge. Added HttpClientModule and UserComponent to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { MypostsComponent } from './myposts/myposts.component';
+import { HttpClientModule } from '@angular/common/http'; 
+
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    appRoutingModule
+    appRoutingModule,
+    HttpClientModule
   ],
 
   declarations: [
     AppComponent,
     ProfileComponent,
     HomeComponent,
-    MypostsComponent
+    MypostsComponent,
+    UsersComponent
   ],
   
   providers: [],
