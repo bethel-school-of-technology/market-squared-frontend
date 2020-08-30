ad402e9b3fa6a2b2d3e2e16a930e26fad0417cf8
fcelestino1031 <felipescelestino@gmail.com>
2020-08-29 20:46:32 -0500

    updated post/user modelsto include spec.ts & routing for create component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,40 @@
-import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { MypostsComponent } from './components/myposts/myposts.component';
 import { PostComponent } from './components/post/post.component';
+import { CreateComponent } from './components/create/create.component';
+
 
 const routes: Routes = [
-    { path: '',redirectTo: 'home', pathMatch: 'full' },
+
+     //localhost:4200/home
     { path: 'home', component: HomeComponent },
+
+    //localhost:4200/profile
     //will need to add :id to end of profile to route to specific profile
     { path: 'profile', component: ProfileComponent },
-    { path: 'myposts', component: MypostsComponent },
-    { path: 'post', component: PostComponent},
+
+
+   //localhost:4200/myposts
+   { path: 'myposts', component: MypostsComponent },
+
+   //localhost:4200/post
+   { path: 'post', component: PostComponent},
+    //localhost:4200/post/1
+    { path: 'post/:postId', component: PostComponent},
+
+    //localhost:4200/create
+    { path: 'create', component: CreateComponent},
+
+    //localhost:4200/
+    { path: '',redirectTo: 'home', pathMatch: 'full' },
+
 
     // otherwise redirect to home
-    //below make a compomonet for 404 route place the 404 componenet below
+    //below make a component for 404 route place the 404 component below
     { path: '**', redirectTo: '' }
 ];
 
