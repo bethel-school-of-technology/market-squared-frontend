91856f820aff46227e0f37430e3bb4759da894ec
Talin Crow <crowtech7@gmail.com>
2020-09-08 14:29:37 -0500

    Formatted some code to make more readable and setting up project for interactive feature on google maps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,32 +10,31 @@ import { CreateComponent } from './components/create/create.component';
 
 const routes: Routes = [
 
-     //localhost:4200/home
+    //localhost:4200/home
     { path: 'home', component: HomeComponent },
 
     //localhost:4200/profile
     //will need to add :id to end of profile to route to specific profile
     { path: 'profile/:id', component: ProfileComponent },
 
+    //localhost:4200/myposts
+    { path: 'myposts', component: MypostsComponent },
 
-   //localhost:4200/myposts
-   { path: 'myposts', component: MypostsComponent },
+    //localhost:4200/post
+    { path: 'post', component: PostComponent },
 
-   //localhost:4200/post
-   { path: 'post', component: PostComponent},
     //localhost:4200/post/1
-    { path: 'post/:postId', component: PostComponent},
+    { path: 'post/:postId', component: PostComponent },
 
     //localhost:4200/create
-    { path: 'create', component: CreateComponent},
+    { path: 'create', component: CreateComponent },
 
     //localhost:4200/
-    { path: '',redirectTo: 'home', pathMatch: 'full' },
-
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
 
     // otherwise redirect to home
     //below make a component for 404 route place the 404 component below
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: 'home' }
 ];
 
 export const appRoutingModule = RouterModule.forRoot(routes);
